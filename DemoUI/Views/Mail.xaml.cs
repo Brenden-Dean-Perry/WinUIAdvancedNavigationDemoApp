fedeed75baade0837e7f4ba2c216622ef8b9e2c3
Brenden Perry <brendenperry5@gmail.com>
2025-06-04 22:57:21 -0700

    Update to AI chat

--- DemoUI/Views/Mail.xaml.cs
+++ DemoUI/Views/Mail.xaml.cs
@@ -15,6 +15,13 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel.Contacts;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
+using Windows.Storage.Provider;
+using Windows.Storage;
+using Microsoft.UI.Text;
+using Windows.UI;
+using WinRT.Interop;
+
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -35,13 +42,15 @@ namespace DemoUI.Views
 
         private void btnSendMessage_Click(object sender, RoutedEventArgs e)
         {
-            Messages.Add(new Message1());
+            editor.Document.GetText(TextGetOptions.NoHidden, out string messageText);
+            Messages.Add(new Message1(messageText, DateTime.Now.ToString("G")));
             InvertedListView.ItemsSource = Messages;
+            editor.Document.SetText(TextSetOptions.None, String.Empty);
         }
         
         private void btnReceiveMessage_Click(object sender, RoutedEventArgs e)
         {
-            Messages.Add(new Message1("Hello", "Test", HorizontalAlignment.Left));
+            Messages.Add(new Message1("Hello", DateTime.Now.ToString("G"), HorizontalAlignment.Left));
             InvertedListView.ItemsSource = Messages;
         }
 
@@ -50,6 +59,120 @@ namespace DemoUI.Views
             var item = (sender as FrameworkElement).DataContext;
             var message = item as Message1;
             Messages.Remove(message);
+        }
+
+
+
+        private async void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Open a text file.
+            Windows.Storage.Pickers.FileOpenPicker open =
+                new Windows.Storage.Pickers.FileOpenPicker();
+            open.SuggestedStartLocation =
+                Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+            open.FileTypeFilter.Add(".rtf");
+
+            Windows.Storage.StorageFile file = await open.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                using (Windows.Storage.Streams.IRandomAccessStream randAccStream =
+                    await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
+                {
+                    // Load the file into the Document property of the RichEditBox.
+                    editor.Document.LoadFromStream(Microsoft.UI.Text.TextSetOptions.FormatRtf, randAccStream);
+                }
+            }
+        }
+
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileSavePicker savePicker = new FileSavePicker();
+            savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+
+            // Dropdown of file types the user can save the file as
+            savePicker.FileTypeChoices.Add("Rich Text", new List<string>() { ".rtf" });
+
+            // Default file name if the user does not type one in or select a file to replace
+            savePicker.SuggestedFileName = "New Document";
+
+            StorageFile file = await savePicker.PickSaveFileAsync();
+            if (file != null)
+            {
+                // Prevent updates to the remote version of the file until we
+                // finish making changes and call CompleteUpdatesAsync.
+                CachedFileManager.DeferUpdates(file);
+                // write to file
+                using (Windows.Storage.Streams.IRandomAccessStream randAccStream =
+                    await file.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite))
+                {
+                    editor.Document.SaveToStream(Microsoft.UI.Text.TextGetOptions.FormatRtf, randAccStream);
+                }
+
+                // Let Windows know that we're finished changing the file so the
+                // other app can update the remote version of the file.
+                FileUpdateStatus status = await CachedFileManager.CompleteUpdatesAsync(file);
+                if (status != FileUpdateStatus.Complete)
+                {
+                    Windows.UI.Popups.MessageDialog errorBox =
+                        new Windows.UI.Popups.MessageDialog("File " + file.Name + " couldn't be saved.");
+                    await errorBox.ShowAsync();
+                }
+            }
+        }
+
+
+        private void BoldButton_Click(object sender, RoutedEventArgs e)
+        {
+            editor.Document.Selection.CharacterFormat.Bold = FormatEffect.Toggle;
+        }
+
+        private void ItalicButton_Click(object sender, RoutedEventArgs e)
+        {
+            editor.Document.Selection.CharacterFormat.Italic = FormatEffect.Toggle;
+        }
+
+        private void ColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Extract the color of the button that was clicked.
+            Button clickedColor = (Button)sender;
+            var rectangle = (Microsoft.UI.Xaml.Shapes.Rectangle)clickedColor.Content;
+            var color = ((Microsoft.UI.Xaml.Media.SolidColorBrush)rectangle.Fill).Color;
+
+            editor.Document.Selection.CharacterFormat.ForegroundColor = color;
+
+            fontColorButton.Flyout.Hide();
+            editor.Focus(Microsoft.UI.Xaml.FocusState.Keyboard);
+        }
+
+        private void FindBoxRemoveHighlights()
+        {
+            ITextRange documentRange = editor.Document.GetRange(0, TextConstants.MaxUnitCount);
+            SolidColorBrush defaultBackground = editor.Background as SolidColorBrush;
+            SolidColorBrush defaultForeground = editor.Foreground as SolidColorBrush;
+
+            documentRange.CharacterFormat.BackgroundColor = defaultBackground.Color;
+            documentRange.CharacterFormat.ForegroundColor = defaultForeground.Color;
+        }
+
+        private void Editor_GotFocus(object sender, RoutedEventArgs e)
+        {
+            editor.Document.GetText(TextGetOptions.UseCrlf, out _);
+
+            // reset colors to correct defaults for Focused state
+            ITextRange documentRange = editor.Document.GetRange(0, TextConstants.MaxUnitCount);
+            SolidColorBrush background = (SolidColorBrush)App.Current.Resources["TextControlBackgroundFocused"];
+
+            if (background != null)
+            {
+                documentRange.CharacterFormat.BackgroundColor = background.Color;
+            }
+        }
+
+        private void Editor_TextChanged(object sender, RoutedEventArgs e)
+        {
+           // throw new NotImplementedException("This method is not implemented yet.");
+           // editor.Document.Selection.CharacterFormat.ForegroundColor = null;// currentColor;
         }
     }
 }
